520c36ef5a20f8edb8da4e9118ad1f4080addeeb
Oleg Gubskiy <gubskyolegod@gmail.com>
2022-12-11 14:09:02 +0200

    end-4-1

--- src/getItemTemplate.js
+++ src/getItemTemplate.js
@@ -22,9 +22,3 @@ export const getItemTemplete = data => {
 //   const lang = Object.values(array).join(', ');
 //   return lang;
 // }
-
-// export const getCounrtyInfoMarkup = data => {
-//   const { flags, name, capital, population, languages } = data;
-//   const lang = Object.values(languages).join(', ');
-//   return `  <div class="card-title">    <img src="${flags.svg}" alt="Country flag" width="40" />    <h>${name}</h>  </div>  <ul class="card-list">    <li class="card-element"><span>Capital:</span>${capital}</li>    <li class="card-element"><span>Population:</span>${population}</li>    <li class="card-element"><span>Languages:</span>${lang}</li>  </ul>  `;
-// };
