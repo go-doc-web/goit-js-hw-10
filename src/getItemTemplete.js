8b7e9ac7d02f82e7fde8f9daea4bd7d0433ac4a6
Oleg Gubskiy <gubskyolegod@gmail.com>
2022-12-11 08:57:39 +0200

    begin-3-1

---
+++ src/getItemTemplete.js
@@ -0,0 +1,12 @@
+export const getItemPreviewTemplete = data => {
+  return data
+    .map(
+      ({ name, flags }) => `
+        <li class="country-item">
+        <img src="${flags.svg}" alt="${name}" width="30" height="20" />
+        <span class="country-name">${name}</span>
+      </li>
+  `
+    )
+    .join('');
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+var debounce = require('lodash.debounce');
+import { getItemPreviewTemplete } from './getItemTemplete';
+
+const URL = 'https://restcountries.com/v2/name/';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+};
+
+const render = list => {
+  refs.list.innerHTML = '';
+  refs.list.insertAdjacentHTML('beforeend', list);
+};
+
+const handleInput = e => {
+  let name = e.target.value;
+  console.log(e.target.value);
+  fetch(`${URL}${name}?fields=name,capital,population,flags,languages`)
+    .then(responce => responce.json())
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        const list = getItemPreviewTemplete(data);
+
+        render(list);
+      }
+      console.log(data);
+    });
+};
+
+refs.input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
